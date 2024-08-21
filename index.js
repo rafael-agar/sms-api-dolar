1d2b6cf00dd8041a3bf240d18d8c80527d15e3ab
Rafael Agar <i.t.rafaelagar@gmail.com>
2024-08-21 12:03:04 -0400

    5pm

--- index.js
+++ index.js
@@ -36,5 +36,4 @@ sendDolarValue()
 
 // Programa la tarea cron para que se ejecute de lunes a viernes a las 5 PM
 // cron.schedule('0 17 * * 1-5', sendDolarValue);
-cron.schedule('11 55 * * 1-5', sendDolarValue);
-
+cron.schedule('* 17 * * 1-5', sendDolarValue);
